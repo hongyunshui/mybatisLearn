8b903acec343ec19c77579aa8751ea591f2a33cd
hongyunshui <jianhong513@163.com>
2019-08-19 12:01:48 +0800

    测试UserDao

---
+++ src/test/java/com/hys/mybatis/dao/imp/UserDaoImpTest.java
@@ -0,0 +1,16 @@
+package com.hys.mybatis.dao.imp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoImpTest {
+	
+	
+
+	@Test
+	public void testFindUserById() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/taotao/controller/imp/PictureControllerTest2.java
@@ -0,0 +1,14 @@
+package com.taotao.controller.imp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PictureControllerTest2 {
+
+	@Test
+	public void testUpLoadPicture() {
+		fail("Not yet implemented");
+	}
+
+}
